51719f3f38111e309f5f41a745abbd6f9d7d749e
Laureen Fernandes <laureenfernandes212@gmail.com>
2020-11-21 23:15:06 +0530

    backend

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 from datetime import datetime
-from flask import Flask,redirect,render_template,url_for
-from flask_login import UserMixin,LoginManager,login_user,logout_user, login_required
+from flask import Flask,redirect,render_template,url_for, request
+from flask_login import UserMixin,LoginManager,login_user,logout_user, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'MYSECRETKEY123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
+
 #login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -18,14 +20,14 @@ login_manager.login_view='login'
 
 @login_manager.user_loader
 def load_user(user_id):
-  return Station.get(user_id)
+  return Station.query.filter_by(id=user_id).first()
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(250))
     place_found = db.Column(db.String(50), nullable=False)
-    date_found = db.Column(db.DateTime, default=datetime.utcnow)
+    date_found = db.Column(db.String(10), nullable=False)
 
     station_id = db.Column(db.Integer, db.ForeignKey('station.id', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
     station = db.relationship('Station')
@@ -47,6 +49,7 @@ class Station(db.Model, UserMixin):
     def check_password(self, password):
          return True if self.password_hash == password else False
 
+db.create_all()
 
 @app.route('/')
 def home():
@@ -54,37 +57,56 @@ def home():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-  # render_template('base.html', current_user=None)
-  form = LoginForm(csrf_enabled=False)
-  if form.validate_on_submit():
+  if request.method == 'POST':
     # query User here:
-    user = Station.query.filter_by(email=form.email.data).first()
+    form = request.form
+    user = Station.query.filter_by(email=form.get('mailid')).first()
     # check if a user was found and the form password matches here:
-    if user and user.check_password(form.password.data):
+    if user and user.check_password(form.get('pass')):
       # login user here:
-      login_user(user, remember=form.remember.data)
-      render_template('index.html',current_user=user)
-      next_page = url_for('home')
-      return redirect(next_page)
+      login_user(user)
+      render_template('enteritem.html')
+      next_page = request.args.get('next')
+      return redirect(next_page) if next_page else redirect(url_for('enteritem'))
     else:
-      return redirect(url_for('login',_external=True))
-  return render_template('login.html', form=form)
+      return redirect(url_for('login', _external=True))
+  return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-  form = RegistrationForm(csrf_enabled=False)
-  if form.validate_on_submit():
-    user = Station(email=form.email.data, name=form.name.data, station=form.station.data)
-    user.set_password(form.password.data)
-    db.session.add(user)
-    db.session.commit()
-  return render_template('register.html', title='Register', form=form) 
+  if request.method == 'POST':
+    # query User here:
+    form = request.form
+    if form.get('pass') == form.get('pass2'):
+      user = Station(email=form.get('mailid'), name=form.get('stationname'), contact_no=form.get('number'), address=form.get('address'))
+      user.set_password(form.get('pass'))
+      db.session.add(user)
+      db.session.commit()
+      return redirect(url_for('login'))
+  return render_template('register.html', title='Register') 
 
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('home'))   
+    return redirect(url_for('home'))
+
+@app.route("/enteritem", methods=['GET', 'POST'])
+@login_required
+def enteritem():
+  if request.method == 'POST':
+    form = request.form
+    item = Item(place_found=form.get('place_found'), station_id=current_user.id, name=form.get('Item_name'), date_found=str(form.get('Date_found')), description=form.get('desc'))
+    db.session.add(item)
+    db.session.commit()
+  return render_template('enteritem.html')
+
+@app.route("/itemview")
+def itemview():
+  items = Item.query.all()
+  search = request.args.get('search')
+  items = Item.query.filter_by(name=search)
+  return render_template('itemview.html', items=items)
 
 if __name__ == '__main__':
     app.run(debug=True)
